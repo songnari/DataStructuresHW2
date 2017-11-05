6780c5182403370ac1e362acc38439975e9e1e0c
songnari <na061300@naver.com>
2017-11-05 17:26:41 +0900

    주석 및 불필요한 출력문 삭제
    계산문 중간에서 /0, %0 으로 에러발생 시 에러 발생 후 나머지 계산을 진행하기 때문에
    ingnore로 남은 입력문을 무시하도록 함.

--- calculator/calculator/main.cpp
+++ calculator/calculator/main.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void calculate(stack<int>& numbers, stack<char>& operations) {
-	//precondition :numbers is not negative integer
+	//precondition :stack is not empty, and numbers is not negative integer
 	//postcondition : result is an integer
 	int number1, number2;
 	char symbol;
@@ -20,10 +20,7 @@ void calculate(stack<int>& numbers, stack<char>& operations) {
 		symbol = operations.top();
 		operations.pop();
 
-		std::cout << "cal : " << number1 << symbol << number2 << endl;
-
-		switch (symbol) {
-			//연산자 확인
+		switch (symbol) {	//연산자 확인
 		case '+':numbers.push(number1 + number2); break;
 		case '-':numbers.push(number1 - number2); break;
 		case '*':numbers.push(number1 * number2); break;
@@ -53,16 +50,10 @@ void calculate(stack<int>& numbers, stack<char>& operations) {
 			else
 				numbers.push(number1 % number2); break;
 		}
-
-		if (numbers.empty() != 1) {
-			if (operations.empty() != 1 && operations.top() == 'E')
-				cout << "EEEEE" << endl;
-			cout << "result : " << numbers.top() << endl;
-		}
 	}
 }
 
-double read_stack(istream& ins) {
+void read_stack(istream& ins) {
 	stack<int> numbers;
 	stack<char> operations;
 	int number;
@@ -70,8 +61,7 @@ double read_stack(istream& ins) {
 
 	while (1) {
 		if (ins.peek() == '0') {
-			cout << "end" << endl;
-			return 0;
+			return ;
 		}
 
 		while (ins && ins.peek() != '\n') {
@@ -80,16 +70,14 @@ double read_stack(istream& ins) {
 				break;
 			if (isdigit(ins.peek())) {
 				ins >> number;
-				std::cout << "number : " << number << endl;
 				numbers.push(number);
 			}
 			else if (strchr("+-*/%^(", ins.peek()) != NULL) {
 				ins >> symbol;
-				std::cout << "operator : " << symbol << endl;
 
 				if (symbol == '(')
 					operations.push(symbol);
-				else if (operations.empty() == 1 || operations.top() == '(') { //stack이 비어있을 때
+				else if (operations.empty() == 1 || operations.top() == '(') { 
 					operations.push(symbol);
 				}
 
@@ -135,8 +123,13 @@ double read_stack(istream& ins) {
 			else
 				ins.ignore();
 		}
-		if (ins.peek() == '\n')
+		while (ins.peek() != '\n') {
 			ins.ignore();
+			
+		}
+		if (ins.peek() == '\n') {
+			ins.ignore();
+		}
 		//입력된 내용 모두 stack 저장 완료
 
 
@@ -148,22 +141,12 @@ double read_stack(istream& ins) {
 
 		if (operations.empty() != 1 && operations.top() == 'E')
 			operations.pop();
+		else
+			cout << numbers.top() << endl;
 	}
-	if (numbers.empty() == 1)
-		return 0;
-	else
-		return 1;
-
 }
 
 int main() {
 
 	read_stack(cin);
 }
-
-//Only non-negative integers can be input as operands
-//number stack , operations stack
-
-//result is always an integer and it can be a negative integer
-
-//When exceptions such as unbalanced parenthesis, "/0" "%0" occur, print "Error!\n" and process next input lines.
